485cf78443ceb0d885b8265e928990df0604dc11
Ashwola <schouderconstant@outlook.com>
2025-02-17 14:10:38 +0100

    Updates on test and add Ruff formating

--- tests/managers/action_manager_test.py
+++ tests/managers/action_manager_test.py
@@ -4,14 +4,16 @@ Created the 08/11/2024
 
 @author: Constant Schouder
 """
+
 from packaging.version import Version
 
 import pytest
-from qtpy import QtWidgets,QtGui,QtCore
+from qtpy import QtWidgets, QtGui, QtCore
 from pymodaq_gui.managers.action_manager import ActionManager
 
 
 version_qt = QtCore.qVersion()
+
 
 @pytest.fixture
 def ini_qt_widget(init_qt):
@@ -22,30 +24,35 @@ def ini_qt_widget(init_qt):
     yield qtbot, widget
     widget.close()
 
-def is_icon_null(action_manager,action_name,):
+
+def is_icon_null(
+    action_manager,
+    action_name,
+):
     action = action_manager.get_action(action_name)
     return action.icon().isNull()
 
 
-
 def test_icon(qtbot):
-    action_manager = ActionManager(toolbar=QtWidgets.QToolBar(),menu=QtWidgets.QMenu())
-    
-    action_manager.add_action(short_name='no_icon', name='my_no_icon', icon_name='')
+    action_manager = ActionManager(toolbar=QtWidgets.QToolBar(), menu=QtWidgets.QMenu())
 
-    assert is_icon_null(action_manager,'no_icon') == True
+    action_manager.add_action(short_name="no_icon", name="my_no_icon", icon_name="")
 
-    action_manager.add_action(short_name='icon_from_pymodaq', name='an_icon_from_pymodaq', icon_name='NewFile')
-    assert is_icon_null(action_manager,'icon_from_pymodaq') == False
+    assert is_icon_null(action_manager, "no_icon")
 
-    if Version(version_qt) > Version('6.7'):
+    action_manager.add_action(
+        short_name="icon_from_pymodaq", name="an_icon_from_pymodaq", icon_name="NewFile"
+    )
+    assert not is_icon_null(action_manager, "icon_from_pymodaq")
 
-        action_manager.add_action(short_name='icon_from_Qt', name='an_icon_from_Qt', icon_name='WindowClose')
-        assert is_icon_null(action_manager,'icon_from_Qt') == False
+    if Version(version_qt) > Version("6.7"):
+        action_manager.add_action(
+            short_name="icon_from_Qt", name="an_icon_from_Qt", icon_name="WindowClose"
+        )
+        assert not is_icon_null(action_manager, "icon_from_Qt")
 
-        icon = QtGui.QIcon.fromTheme(QtGui.QIcon.ThemeIcon.WindowClose)        
-        action_manager.add_action(short_name='icon', name='an_icon_from_Qt', icon_name=icon)
-        assert is_icon_null(action_manager,'icon') == False
-
-
-
+        icon = QtGui.QIcon.fromTheme(QtGui.QIcon.ThemeIcon.WindowClose)
+        action_manager.add_action(
+            short_name="icon", name="an_icon_from_Qt", icon_name=icon
+        )
+        assert not is_icon_null(action_manager, "icon")
